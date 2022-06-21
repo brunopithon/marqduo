3aaae4e76dc144837813acdf4c47eed583eb330d
brunopithon <brunopithondev@gmail.com>
2022-06-21 20:58:41 -0300

    Criando o controler para a pagina inicial e configurando para tela de inicio com o root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :suppliers
   resources :service_providers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'welcome#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
