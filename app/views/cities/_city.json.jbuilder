ebf2832826c649cd48f5e3ead91e9f6b94228f53
brunopithon <brunopithondev@gmail.com>
2022-06-21 21:29:01 -0300

    Criando o crud com o scaffold para o cadastro das cidades

---
+++ app/views/cities/_city.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! city, :id, :city_name, :state, :created_at, :updated_at
+json.url city_url(city, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_233642) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_002713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "city_name"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "service_providers", force: :cascade do |t|
     t.string "person"
